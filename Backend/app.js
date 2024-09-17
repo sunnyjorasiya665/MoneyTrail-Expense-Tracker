40b2908cccff7000d50007b0fd66e0c6630cd583
SUNNY JORASIYA <118279555+sunnyjorasiya665@users.noreply.github.com>
2024-09-17 17:11:20 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -16,7 +16,7 @@ mongoose
   .catch((e) => console.log(e));
 
 //!Cors Configuratin
-const corsOptions = { origin: ["https://money-trail-tracker-cdjugy7lm-sunny-jorasiyas-projects.vercel.app/"] };
+const corsOptions = { origin: ["https://moneytrail-expense-income-tracker-5pnupyjis.vercel.app"] };
 app.use(cors(corsOptions));
 
 //!Middlewares
