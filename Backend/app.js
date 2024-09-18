11a4284b9e71885f179056b2e132e1ab24c03ecc
SUNNY JORASIYA <118279555+sunnyjorasiya665@users.noreply.github.com>
2024-09-19 01:53:44 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -16,7 +16,7 @@ mongoose
   .catch((e) => console.log(e));
 
 //!Cors Configuratin
-const corsOptions = { origin: ["http://localhost:5173"] };
+const corsOptions = { origin: ["https://moneytrail.vercel.app","https://moneytrail-tracker.vercel.app/"] };
 app.use(cors(corsOptions));
 
 //!Middlewares
